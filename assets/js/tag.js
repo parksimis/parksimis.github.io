157b35dac4448a77ab11c04b33734c4d0f6b07a9
Park Seon Ik <parksimis@gmail.com>
2022-04-21 18:07:48 +0900

    Modify : Tag

--- assets/js/tag.js
+++ assets/js/tag.js
@@ -3,8 +3,10 @@
 function tmp(e){
     let dv = event.currentTarget;
     currentTag = dv.getAttribute("data-tag");
-    alert(currentTag);
-    filterByTagName(currentTag);
+    alert(dv);
+    var tmp = $(".post_li").getAttribute("post-jekyll");
+    alert(tmp);
+    // filterByTagName(currentTag);
 }
 
 $("[data-tag]").click((e) => {
@@ -16,7 +18,7 @@ $("[data-tag]").click((e) => {
 function filterByTagName(tagName) {
   $('.hidden').removeClass('hidden');
   $('.post_li').each((index, elem) => {
-    if (!elem.hasAttribute(`data-${tagName}`)) {
+    if (!elem.childNodes.hasAttribute(`data-${tagName}`)) {
       $(elem).addClass('hidden');
     }
   });
